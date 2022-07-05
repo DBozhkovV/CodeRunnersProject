452a8b68b3263e398a29d963911bd27fd2ff2c72
Liverpoolfan92 <zhelev.burislav@gmail.com>
2022-07-05 21:45:25 +0300

    seat_table_added

---
+++ ProjectAPI/ProjectAPI/Data/Models/Seat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ProjectAPI.Data.Models
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string Color { get; set; }
+        public Seat()
+        {
+
+        }
+    }
+}
