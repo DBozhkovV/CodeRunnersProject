cc6839e75f963ad56ce2bd1524c3fd0a7f05a1d7
horhe579 <georgi.i.traykov.2018@elsys-bg.org>
2022-07-06 14:46:21 +0300

    Seat Controller Get start
    get

---
+++ ProjectAPI/ProjectAPI/Controllers/SeatController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectAPI.Context;
+using ProjectAPI.Data.Models;
+
+namespace ProjectAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SeatController : Controller
+    {
+        private readonly AppDbContext _DbContext;
+
+        public SeatController(AppDbContext testDBContext)
+        {
+            _DbContext = testDBContext;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get()
+        {
+            var testData = _DbContext.Seats.Single(x => x.Name == "name 1");
+            // db.Seats.
+            var test = new Seat
+            {
+                Id = 1,
+                Name = "name 1"
+            };
+
+            return Ok(test);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
