730b9271229bcbdad98b085e01f3f33540a7778d
Hristiyyann <80455190+Hristiyyann@users.noreply.github.com>
2022-07-08 13:44:10 +0300

    Success page added

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import Admin from "./components/pages/Admin";
 import Profile from "./components/pages/Profile";
+import Success from "./components/pages/Success";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/register" element={<Register/>} />
           <Route path="/admin" element={<Admin/>} />
           <Route path="/profile" element={<Profile/>} />
+          <Route path="/success" element={<Success/>} />
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ ClientApp/src/components/pages/Success.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect }from 'react';
+import {Helmet} from "react-helmet";
+import classes from './Success.module.css'
+import photo from '../images/banana.svg'
+
+function Success() 
+{
+    const[isSecondLottieLoaded, setSecondLottieLoaed] = useState(false);
+
+    useEffect(() => 
+    {
+        const timer = setTimeout(() => setSecondLottieLoaed(true), 3000);
+    }, []);
+
+    return(
+        <div className = {classes.content}>
+            <Helmet>
+                <script
+                src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"
+                async
+                ></script>
+            </Helmet>
+            <div className = {classes.success}>
+                <h1 className = {classes.text}>You have been registered successfully!</h1>
+                {isSecondLottieLoaded && <lottie-player className={classes.lottie} src="https://assets8.lottiefiles.com/packages/lf20_bhths1ye.json"  background="transparent"  speed="1"  style={{width: "400px", height: "400px"}}  autoplay></lottie-player>}
+                {!isSecondLottieLoaded && <lottie-player className={classes.lottie} src="https://assets1.lottiefiles.com/packages/lf20_y9jgwjuc.json"  background="transparent"  speed="0.7"  style={{width: "350px", height: "400px"}} autoplay></lottie-player>}
+            </div>
+        </div>
+    ) 
+
+}
+
+export default Success;
