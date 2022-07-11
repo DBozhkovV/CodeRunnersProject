242d9600efac7e6813637d541f991c09eb233b54
Hrist0ff <hristoffcsgoplayer@gmail.com>
2022-07-11 11:21:16 +0300

    Check if two passwords match in Register form

--- ClientApp/src/components/pages/Register.js
+++ ClientApp/src/components/pages/Register.js
@@ -1,53 +1,87 @@
-import React, { useRef } from "react";
-import axios from 'axios';
-// !!! PROBLEM KOGATO SE REGISTRIRA URL-A se smenq na dannite na registriraliqt se
-// !!! PROVERKA DALI REPASSWORD-A suvpada s passworda
+import React, { useState, useRef,useEffect } from "react";
+
+
 
 function Register() {
 
-    const firstNameField = useRef();
-    const lastNameField = useRef();
-    const passwordField = useRef();
-    const rePasswordField = useRef();
-    const emailField = useRef();
+    const [pass, setPassword] = useState('');
+    const [cPassword, setCPassword] = useState('');
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const [cPasswordClass, setCPasswordClass] = useState('form-control');
+    const [isCPasswordDirty, setIsCPasswordDirty] = useState(false);
+
+    const firstName = useRef();
+    const lastName = useRef();
+    const password = useRef();
+    const rePassword = useRef();
+    const email = useRef();
+
+    const handleCPassword = (e) => {
+        setCPassword(e.target.value);
+        setIsCPasswordDirty(true);
+    }
+
+    useEffect(() => {
+        if (isCPasswordDirty) {
+            if (pass === cPassword) {
+                setShowErrorMessage(false);
+                setCPasswordClass('form-control is-valid')
+            } else {
+                setShowErrorMessage(true)
+                setCPasswordClass('form-control is-invalid')
+            }
+        }
+    }, [cPassword])
 
     const register = (event) => {
        
         event.preventDefault();
-        const name = firstNameField.current.value;
-        // const enteredlastName = lastName.current.value;
-        const email = emailField.current.value;
-        const password = passwordField.current.value;
-        // const enteredRePassword = rePassword.current.value;
+        const enteredFirstName = firstName.current.value;
+        const enteredlastName = lastName.current.value;
+        const enteredEmail = email.current.value;
+        const enteredPassword = password.current.value;
+        const enteredRePassword = rePassword.current.value;
 
-        axios.post(`https://localhost:7031/Register`, {email, password, name})
-            .catch(error => {
-                console.log(error)
-            })
+        const requestOptions = {
+            method: 'POST', headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({
+                firstName: enteredFirstName, 
+                lastName: enteredlastName,
+                password: enteredPassword,
+                rePassword: enteredRePassword,
+                email: enteredEmail
+            }),
+            mode: 'cors'   
+        };     
+
+        console.log(requestOptions);
+
+        firstName.current.value = lastName.current.value = email.current.value = password.current.value = rePassword.current.value = "";
     }
 
     return(
         <div className="container mt-5">
+
             <h1>Sign up</h1>
             <p>Create your account</p>
             <form>
               <div className="form-group">
                 <input 
                     className="form-control"
-                    type="text"
+                    type="firstName"
                     placeholder="Enter first name"
                     name="firstName"
-                    ref = {firstNameField}
+                    ref = {firstName}
                     required
                 />
               </div>
               <div className="form-group">
                 <input 
                     className="form-control"
-                    type="text"
+                    type="lastName"
                     placeholder="Enter last name"
                     name="lastName"
-                    ref = {lastNameField}
+                    ref = {lastName}
                     required
                 />
               </div>
@@ -57,18 +91,21 @@ function Register() {
                       type="email"
                       placeholder="Enter email"
                       name="email"
-                      ref = {emailField}
+                      ref = {email}
                       required
                   />
               </div>
               <div className="form-group">
                   <input 
                       className="form-control"
+                      id="pswd"
                       type="password"
                       placeholder="Enter password"
                       name="password"
+                      value={pass}
+                      onChange={(e) => { setPassword(e.target.value) }}
                       minLength='8'
-                      ref = {passwordField}
+                      ref = {password}
                       required
                 />
               </div>
@@ -76,14 +113,18 @@ function Register() {
                   <input 
                       className="form-control"
                       type="password"
+                      id="repswd"
                       placeholder="Confirm password"
+                      value={cPassword}
+                    onChange={handleCPassword}
                       name="re_password"
                       minLength='8'
-                      ref = {rePasswordField}
+                      ref = {rePassword}
                       required
                 />
               </div>
-            <button className="btn btn-primary" type="submit" onClick={register}>Register</button>
+              {showErrorMessage && isCPasswordDirty ? <div> Passwords did not match </div> : ''}
+            <button disabled={showErrorMessage && isCPasswordDirty ? true : false}className="btn btn-primary" type="submit" onClick={register}>Register</button>
             </form>
         </div>
     );
