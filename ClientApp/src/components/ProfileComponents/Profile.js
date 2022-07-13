b3a36c5976df18951d2e936f59ec26e5f24ad155
Hrist0ff <hristoffcsgoplayer@gmail.com>
2022-07-13 15:58:47 +0300

    Change Image from Profile page

--- ClientApp/src/components/ProfileComponents/Profile.js
+++ ClientApp/src/components/ProfileComponents/Profile.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import classes from './Profile.module.css'
 import jwt from 'jwt-decode';
 import axios from 'axios';
+import '../../components/App.css';
 import { Link } from 'react-router-dom';
 
 function Profile() {
     const [user, setUser] = useState([]);
     const [age, setAge] = useState();
+    const [imageUrl, setImageUrl] = useState();
     const [phoneNumber,setPhoneNumber] = useState();
     const t = jwt(localStorage.getItem("token"));
     const token = localStorage.getItem("token");
@@ -17,10 +19,13 @@ function Profile() {
 
     const targetDiv = document.getElementById("changeAge");
     const targetDiv2 = document.getElementById("changeNumber");
+    const targetDiv3 = document.getElementById("changeImage");
+
     const btn = document.getElementById("ca");
     useEffect(() => {
         document.getElementById("changeAge").style.display = "none";
         document.getElementById("changeNumber").style.display = "none";
+        document.getElementById("changeImage").style.display = "none";
     }, []);
 
   
@@ -39,6 +44,14 @@ function Profile() {
           targetDiv2.style.display = "none";
         } else {
           targetDiv2.style.display = "block";
+        }
+    };
+
+    function btnfunc3() {
+        if (targetDiv3.style.display !== "none") {
+          targetDiv3.style.display = "none";
+        } else {
+          targetDiv3.style.display = "block";
         }
     };
 
@@ -64,6 +77,7 @@ function Profile() {
         window.location.reload();
     }
 
+
     const changeNumber = async () => {
         axios.put(`https://localhost:7031/Register/phone?phone=${phoneNumber}&id=${user.id}`,{},{ headers: {"Authorization" : `Bearer ${token}`} })
             .catch(error => {
@@ -72,21 +86,40 @@ function Profile() {
         window.location.reload();
     }
 
+    const changeImage = async () => {
+        axios.put(`https://localhost:7031/Register/image?image=${imageUrl}&id=${user.id}`,{},{ headers: {"Authorization" : `Bearer ${token}`} })
+            .catch(error => {
+                console.log(error)
+            })
+        window.location.reload();
+    }
+
+
     return(
         <div className={classes.background}>
+            <div>
             <Link to={`/`} className="btn btn-outline-dark me-2">Go back</Link>
             <button className="btn btn-outline-dark me-2 float-right" id="toggle" onClick={btnfunc}>Change age</button>
+            <button className="btn btn-outline-dark me-2 float-right" id="toggle1" onClick={btnfunc2}>Change phone number</button>
+            <button className="btn btn-outline-dark me-2 float-right" id="toggle1" onClick={btnfunc3}>Change Image (URL)</button>
+            </div>
+        <div id="wrapper">
             <div id="changeAge">
                 <input type="text" placeholder="Enter age" id="inpAge" onChange={(e) => { setAge(e.target.value) }}/>
                 <button onClick={changeAge}>Submit</button>
-            </div>
+            </div>           
             
-            <button className="btn btn-outline-dark me-2 float-right" id="toggle1" onClick={btnfunc2}>Change phone number</button>
             <div id="changeNumber">
                 <input type="text" placeholder="Enter phone number" id="inpAge" onChange={(e) => { setPhoneNumber(e.target.value) }}/>
                 <button onClick={changeNumber}>Submit</button>
             </div>
 
+            <div id="changeImage">
+                <input type="text" placeholder="Enter Image URL" id="inpAge" onChange={(e) => { setImageUrl(e.target.value) }}/>
+                <button onClick={changeImage}>Submit</button>
+            </div>
+
+        </div>
             <div className = {classes.card}>
                 <div className = {classes.content}>
                     <img className = {classes.photo} src={user.image} alt = "Profile pic"></img>
@@ -98,10 +131,7 @@ function Profile() {
                         <h2><b>Email</b></h2>
                         <h4>{user.email}</h4>
                     </div>
-                    <div className = {classes.field}>
-                        <h2><b>User ID</b></h2>
-                        <h4>{user.id}</h4>
-                    </div>
+
                     <div className = {classes.field}>
                         <h2><b>Position</b></h2>
                         <h4>{user.position}</h4>  
